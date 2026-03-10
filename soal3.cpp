fd50f929512d76c4a39e680e8ff4b45d9fb4fd94
R471QUL <raziqlgs7@gmail.com>
2026-03-10 22:56:17 +0700

    Implement time conversion from seconds to H:M:S format

--- soal3.cpp
+++ soal3.cpp
@@ -4,3 +4,35 @@
     Buatlah program yang menerima input satu bilangan bulat (detik) 
     dan menampilkan output dalam format: X detik = H jam M menit S detik
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int detik;
+    int jam, menit, sisaDetik;
+
+    // Meminta input jumlah detik
+    cout << "Masukkan jumlah detik: ";
+    cin >> detik;
+
+    // Menghitung jam
+    jam = detik / 3600;
+
+    // Menghitung sisa detik setelah diambil jam
+    sisaDetik = detik % 3600;
+
+    // Menghitung menit
+    menit = sisaDetik / 60;
+
+    // Menghitung detik tersisa
+    sisaDetik = sisaDetik % 60;
+
+    // Menampilkan hasil
+    cout << detik << " detik = "
+         << jam << " jam "
+         << menit << " menit "
+         << sisaDetik << " detik" << endl;
+
+    return 0;
+}
